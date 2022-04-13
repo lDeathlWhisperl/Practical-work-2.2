ecbb58f94e1474147dbbfe0f63b4fcaa0624c831
Илья Митин <iliya2001@mail.ru>
2022-04-14 02:18:35 +0300

    little bugfix

--- Practical work 2.2/main.cpp
+++ Practical work 2.2/main.cpp
@@ -4,24 +4,7 @@
 
 int main()
 {
-	//fullscreen();
-	//mainMenu();
-    
-    List L;
-
-    const int n = 5;
-    int a[n] = { 0,1,2,3,4 };
-
-    for (int i = 0; i < n; i++)
-        if (i % 2 == 0)
-            L.addHead(a[i]);
-        else
-            L.addTail(a[i]);
-
-    std::cout << "\n";
-    L.print();
-    std::cout << "\n";
-    L.swap(0, 4); //std::cout << "   ";
-    L.print();// std::cout << L.getSize();
+	fullscreen();
+	mainMenu();
     return 0;
 }
